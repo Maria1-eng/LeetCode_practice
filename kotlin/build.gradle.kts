696da107c1a48b9e97283476da27922be33c2b96
Tyler-Lopez <77797048+Tyler-Lopez@users.noreply.github.com>
2025-08-26 20:57:12 -0600

    #11 Add Kotlin entry-point
    This commit adds a Kotlin folder to the repository, which comes with the files necessary to more easily run Kotlin code and test it without needing to rely so heavily on LeetCode.

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
